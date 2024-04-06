7abb556789b22261e253c5b8b65b64c71c4975d7
abhitech2002 <oabhishek31@gmail.com>
2024-04-06 22:21:29 +0530

    feat: Add MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from 'express'
+import mongoose from 'mongoos'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+// Middleware
+app.use(express.json())
+
+// Mongoose for connecting to mongoDB Database
+const MONGODB_URI = process.env.MONGODB_URI
+mongoose
+    .connect(MONGODB_URI)
+    .then( () => {
+        console.log("MongoDB connected")
+    })
+    .catch((error) => console.log(error))
 
 // Hello World Testing 
 app.get("/", (req, res) => {
